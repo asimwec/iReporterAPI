9d51fc038f8450d535e46504005f945a20fe8dcc
simon peter <asimwecrystal@gmail.com>
2018-12-06 14:58:53 +0300

    edited the test file adding the user and red flag defs

--- services.py
+++ services.py
@@ -7,11 +7,83 @@ import requests
 
 from constants import BASE_URL
 
-HEROKU_URL = urljoin(BASE_URL, 'incidents')
+all_incidents = urljoin(BASE_URL, 'incidents')
+all_red_flags = urljoin(BASE_URL, 'red-flags')
+all_red_flags_by_id = urljoin(BASE_URL, 'red-flags/3')
+all_interventions = urljoin(BASE_URL, 'interventions')
+all_interventions_by_id = urljoin(BASE_URL, 'interventions/2')
+all_users = urljoin(BASE_URL, 'users')
+all_users_by_id = urljoin(BASE_URL, 'users/1')
+all_red_flags_for_users_by_id = urljoin(BASE_URL, 'users/1/red-flags')
+all_interventions_for_users_by_id = urljoin(BASE_URL, 'users/2/interventions')
 
 
-def get_heroku():
-    response = requests.get(HEROKU_URL)
+def get_all_incidents():
+    response = requests.get(all_incidents)
+    if response.ok:
+        return response
+    else:
+        return None
+
+
+def get_all_red_flags():
+    response = requests.get(all_red_flags)
+    if response.ok:
+        return response
+    else:
+        return None
+
+
+def get_all_red_flags_by_id():
+    response = requests.get(all_red_flags_by_id)
+    if response.ok:
+        return response
+    else:
+        return None
+
+
+def get_all_interventions():
+    response = requests.get(all_interventions)
+    if response.ok:
+        return response
+    else:
+        return None
+
+
+def get_all_interventions_by_id():
+    response = requests.get(all_interventions_by_id)
+    if response.ok:
+        return response
+    else:
+        return None
+
+
+def get_all_users():
+    response = requests.get(all_users)
+    if response.ok:
+        return response
+    else:
+        return None
+
+
+def get_all_users_by_id():
+    response = requests.get(all_users_by_id)
+    if response.ok:
+        return response
+    else:
+        return None
+
+
+def get_all_red_flags_for_users_by_id():
+    response = requests.get(all_red_flags_for_users_by_id)
+    if response.ok:
+        return response
+    else:
+        return None
+
+
+def get_all_interventions_for_users_by_id():
+    response = requests.get(all_interventions_for_users_by_id)
     if response.ok:
         return response
     else:
--- test_iReporterApi.py
+++ test_iReporterApi.py
@@ -1,10 +1,51 @@
 from nose.tools import assert_is_not_none
-from services import get_heroku
+from services import get_all_incidents, get_all_red_flags, get_all_interventions, get_all_users, \
+    get_all_red_flags_by_id, get_all_interventions_by_id, get_all_users_by_id, get_all_red_flags_for_users_by_id, \
+    get_all_interventions_for_users_by_id
 
 
-def test_request_response():
-    # Call the service, which will send a request to the server.
-    response = get_heroku()
-
-    # If the request is sent successfully, then I expect a response to be returned.
+def test_get_all_incidents():
+    response = get_all_incidents()
     assert_is_not_none(response)
+
+
+def test_get_all_red_flags():
+    response = get_all_red_flags()
+    assert_is_not_none(response)
+
+
+def test_get_all_red_flags_by_id():
+    response = get_all_red_flags_by_id()
+    assert_is_not_none(response)
+
+
+def test_get_all_interventions():
+    response = get_all_interventions()
+    assert_is_not_none(response)
+
+
+def test_get_all_interventions_by_id():
+    response = get_all_interventions_by_id()
+    assert_is_not_none(response)
+
+
+def test_get_all_users():
+    response = get_all_users()
+    assert_is_not_none(response)
+
+
+def test_get_all_users_by_id():
+    response = get_all_users_by_id()
+    assert_is_not_none(response)
+
+
+def test_get_all_red_flags_for_users_by_id():
+    response = get_all_red_flags_for_users_by_id()
+    assert_is_not_none(response)
+
+
+def test_get_all_interventions_for_users_by_id():
+    response = get_all_interventions_for_users_by_id()
+    assert_is_not_none(response)
+
+
